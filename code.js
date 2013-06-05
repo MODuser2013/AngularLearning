43a500d3d93300d67eb4dc1868d372134fc69e76
Brandon Estock <brandon.estock@oasisdigital.com>
2013-06-05 14:48:12 -0500

    HUH?

--- code.js
+++ code.js
@@ -1,5 +1,20 @@
 
 
 function Encrypt($scope){
-       //Hi alex
+    $scope.phrase = "Default";
+    $scope.pass = "Default";
+    $scope.result = "";
+    $scope.gibberish = new function() {
+        $scope.result = "";
+        for(var x = 0; x < phrase.length; x++) {
+            var tempC;
+            for(var y = 0; y < pass.length; y++) {
+                var temp = (($scope.phrase[x].charCode + $scope.pass[y].charCode));
+                if(temp > 255){temp-=255;}
+                tempC = String(temp);
+            }
+            $scope.result+=tempC;
+        }
+
+    } ;
 }
