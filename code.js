a745abeed14bd210f52c27d6e8405d8dcb84f757
Brandon Estock <brandon.estock@oasisdigital.com>
2013-06-05 15:08:24 -0500

    it kinda works

--- code.js
+++ code.js
@@ -4,17 +4,20 @@ function Encrypt($scope){
     $scope.phrase = "Default";
     $scope.pass = "Default";
     $scope.result = "";
-    $scope.gibberish = new function() {
+    $scope.gibberish = function() {
+        //alert("");
         $scope.result = "";
-        for(var x = 0; x < phrase.length; x++) {
-            var tempC;
-            for(var y = 0; y < pass.length; y++) {
-                var temp = (($scope.phrase[x].charCode + $scope.pass[y].charCode));
-                if(temp > 255){temp-=255;}
-                tempC = String(temp);
+        for(var x = 0; x < $scope.phrase.length; x++) {
+            var tempC = "";
+            for(var y = 0; y < $scope.pass.length; y++) {
+                var temp = (($scope.phrase.charCodeAt(x) + $scope.pass.charCodeAt(y)));
+               if(temp > 255){temp-=255;}
+               tempC = String.fromCharCode(Number(temp));
             }
             $scope.result+=tempC;
         }
-
+        //console.log($scope);
+        //console.log($scope.phrase.charCodeAt(0));
     } ;
+
 }
