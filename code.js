ea19f952234dff065122046285f79d568026f07d
Brandon Estock <brandon.estock@oasisdigital.com>
2013-06-06 11:20:08 -0500

    made the code correct for angular (basicly just reformated it)

--- code.js
+++ code.js
@@ -1,25 +1,21 @@
 
+   encryptApp = angular.module('encryptApp',[])
+       .controller('encryptController',function($scope){
+           $scope.data = {phrase: 'Default', mod: 'Default'};
+           $scope.settings = {maxKeySpace: 122, minKeySpace: 21, offset: 0};
 
-function Encrypt($scope){
-    $scope.phrase = "Default";
-    $scope.pass = "Default";
-    $scope.result = "";
-    $scope.gibberish = function() {
-        //alert("");
-        $scope.result = "";
-        for(var x = 0; x < $scope.phrase.length; x++) {
-            var tempC = "";
-            for(var y = 0; y < $scope.pass.length; y++) {
-                var temp = (($scope.phrase.charCodeAt(x) + $scope.pass.charCodeAt(y)));
-               if(temp < 21){temp+=21;}
-               if(temp > 122){temp-=122;}
-               tempC = String.fromCharCode(Number(temp));
-            }
-            $scope.result+=tempC;
-        }
-        //console.log($scope);
-        console.log($scope.result.length + " = " + $scope.phrase.length);
-        //console.log($scope.phrase.charCodeAt(0));
-    } ;
-
-}
+           $scope.data.modify = function() {
+             var result = "";
+               for(var x = 0; x < $scope.data.phrase.length; x++) {
+                   var tempC = "";
+                   for(var y = 0; y < $scope.data.mod.length; y++) {
+                       var temp = (($scope.data.phrase.charCodeAt(x) + $scope.data.mod.charCodeAt(y)));
+                       if(temp < $scope.settings.minKeySpace){temp+=$scope.settings.minKeySpace;}
+                       if(temp > $scope.settings.maxKeySpace){temp-=$scope.settings.maxKeySpace;}
+                       tempC = String.fromCharCode(Number(temp));
+                   }
+                   result+=tempC;
+               }
+               return result;
+           };
+       });
